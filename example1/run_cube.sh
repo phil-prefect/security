dbcfad50385e14ed0913b4db063fade5d274fdf3
phil-prefect <183716360@qq.com>
2020-06-19 01:37:53 +0800

    del

--- example1/run_cube.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export CUBE_PATH=/home/student/cube-1.3
-export CUBEAPPPATH=/home/student/example 
-export LD_LIBRARY_PATH=$CUBE_PATH/cubelib/lib/:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$CUBEAPPPATH/locallib/bin/:$LD_LIBRARY_PATH
-$CUBE_PATH/proc/main/envset_proc $1 $2 $3 $4 $5 $6
