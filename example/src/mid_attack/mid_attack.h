9d0ccd760e9edda79620f5abc16e2f22847369ef
phil-prefect <183716360@qq.com>
2020-06-19 01:40:31 +0800

    control

---
+++ example/src/mid_attack/mid_attack.h
@@ -0,0 +1,8 @@
+#ifndef MID_ATTACK_FUNC_H
+#define MID_ATTACK_FUNC_H
+
+// plugin's init func and kickstart func
+int mid_attack_init(void * sub_proc,void * para);
+int mid_attack_start(void * sub_proc,void * para);
+
+#endif
